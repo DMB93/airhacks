448c86ce708aed4f6b7610de5962bb2217cf9d65
Adam Bien <abien@adam-bien.com>
2018-02-20 11:51:53 +0100

    input type event processed

--- calculation-ui/src/app.js
+++ calculation-ui/src/app.js
@@ -1,13 +1,21 @@
-const small = document.querySelector("small");
-console.dir(small);
+class CalculationView { 
 
-small.innerText = "hugo";
-const dynamic = "something"
-const java = document.querySelector("#java");
-java.innerHTML = `<p>
-${dynamic}
-</p>
-`;
+    constructor() { 
+        this.small = document.querySelector("small");
+        this.inputText = document.querySelector("input");
+        this.button = document.querySelector("button");
+        this.button.onclick = () => this.something();
+        console.dir(this.inputText);
+        this.inputText.onchange = e => this.small.innerText = e.target.value;
+    }
+
+    something() { 
+        const p = document.createElement("div");
+        p.innerText = "new";
+        this.small.appendChild(p);
+    }
+
+}
 
 
-
+window.view = new CalculationView();
