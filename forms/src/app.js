112e45229c319a8d8ea7e443c59fb2a21fda01a7
Adam Bien <abien@adam-bien.com>
2018-02-22 09:52:38 +0100

    all input fields read

--- forms/src/app.js
+++ forms/src/app.js
@@ -2,13 +2,15 @@ class FormView {
     constructor() { 
         this.form = document.querySelector("form");
         this.form.onsubmit = e => this.onMySubmit(e);
-        this.lastNameText = document.querySelector("#lastName");
+        this.lastNameText = document.querySelectorAll("input");
     }
 
     onMySubmit(event) { 
         event.preventDefault();
         console.log(event);
-        
+        console.dir(this.lastNameText);
+
+        this.lastNameText.forEach(e => console.log(e.value));
     }
 }
 
